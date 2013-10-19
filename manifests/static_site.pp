d3399d76e1288bd84634b919bc6d3cd9f0c74969
Tom Booth <tombooth@gmail.com>
2013-10-19 19:39:17 +0100

    Add /var/log symlink

--- manifests/static_site.pp
+++ manifests/static_site.pp
@@ -16,6 +16,11 @@ define tombooth::static_site(
     ensure => directory,
   }
 
+  file { "/var/log/static-${name}":
+    ensure => link,
+    target => "/var/www/${name}/logs",
+  }
+
   nginx::resource::vhost { $name:
     server_name => $server_name,
     www_root    => "/var/www/${name}/current",
